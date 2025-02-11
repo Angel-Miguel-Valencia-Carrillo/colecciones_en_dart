77c6a2010b7eff46f364f358a250a52e75f8299a
Angel-Miguel-Valencia-Carrillo <a.22308051280738@cbtis128.edu.mx>
2025-02-11 17:00:18 +0000

    ejemplo de lista en github

--- lista1.dart
+++ lista1.dart
@@ -1,5 +1,6 @@
 void main() {  
   // lista tipo entero de 3 elentos
+  print("Angel Valencia 22308051280738 gpo 6 J");
    List<int> numeros= [10, 20, 30];
    print("lista de numeros enteros: ");
    print(numeros);   
